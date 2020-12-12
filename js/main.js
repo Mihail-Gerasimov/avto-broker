6fb9f950ce5f338ec60710940565d5a471e6e6b1
Mihail <mihailgerasimov86@gmail.com>
2020-12-12 14:31:15 +0400

    'mobile-card'

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-// $('.multiple-items').slick({
-//     infinite: true,
-//     dots: false,
-//     slidesToShow: 3,
-//     slidesToScroll: 1,
-//     arrows: false
-// });
+$(document).ready(function() {
+    // slider
+    if ($(window).width() < 426) {
+
+        $('.multiple-items').slick({
+            infinite: false,
+            dots: false,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false,
+            centerMode: true,
+            centerPadding: '30px'
+        });
+    }
+    // $('.advantages__card:nth-child(2) .crad--picture').addClass("mb-3");
+});
